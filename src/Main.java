a98ae93ed0131c20e03883adf2e213dae737afef
romnar <romain.narboni@isep.fr>
2017-05-13 19:53:51 +0200

    affectation de plusieurs Id par Station

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,7 @@ public class Main {
 			List<String> lines = readFile.readWithStop("documents/RATP_GTFS_METRO_" + str + "/stops.txt");
 			ArrayList<Stop> listStops = graph.createListOfStations(lines);
 			for(Stop stop : listStops) {
-				System.out.println("Station : " + stop.getName());
+				System.out.println("Station : " + stop.getName() + ", Id : " + stop.getId());
 			}
 		}
 		System.out.println("Nombre de Stations : " + graph.getNumberOfStopInParis());
--- src/UnweightedGraph.java
+++ src/UnweightedGraph.java
@@ -8,10 +8,11 @@ import java.util.Map.Entry;
 public class UnweightedGraph {
 	ArrayList<Stop> stops = new ArrayList<Stop>();
 	ArrayList<String> nameStations = new ArrayList<String>();
-	//Map.Entry<String,List<Long>> map = (Entry<String, List<Long>>) new HashMap<String, List<Long>>();
+	Map<String,List<Long>> map = new HashMap<String, List<Long>>();
 	
 	public ArrayList<Stop> createListOfStations(List<String> lines) {
-		//List<Long> idList = new ArrayList<Long>();
+		ArrayList<Map<String,Long>> idList = new ArrayList<Map<String,Long>>();
+		
 		lines.remove(0);
 		String newLine;
 		for (String line : lines) {
@@ -26,11 +27,21 @@ public class UnweightedGraph {
 			stop.setName(parametersOfStation.get(2).replace("\"", ""));
 			stop.setDescription(parametersOfStation.get(3).replace("\"", ""));
 			stop.setLatitude(Float.parseFloat(parametersOfStation.get(4)));
-			stop.setLongitude(Float.parseFloat(parametersOfStation.get(5)));			
-			//stop.setId(Long.parseLong(parametersOfStation.get(0)));
-						
+			stop.setLongitude(Float.parseFloat(parametersOfStation.get(5)));
+
+			
+			
+			if(map.containsKey(parametersOfStation.get(2).replace("\"", ""))) {
+				map.get(parametersOfStation.get(2).replace("\"", "")).add(Long.parseLong(parametersOfStation.get(0)));			
+			} else {
+				List<Long> test = new ArrayList<Long>();
+				test.add(Long.parseLong(parametersOfStation.get(0)));
+				map.put(parametersOfStation.get(2).replace("\"", ""),test);
+			}
+
 			if(!nameStations.contains(parametersOfStation.get(2).replace("\"", ""))) {
-				nameStations.add(parametersOfStation.get(2).replace("\"", ""));
+				nameStations.add(parametersOfStation.get(2).replace("\"", ""));	
+				stop.setId(map.get(parametersOfStation.get(2).replace("\"", "")));
 				getListStops().add(stop);
 			}
 	    }
